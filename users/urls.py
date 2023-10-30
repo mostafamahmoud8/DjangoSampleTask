067dbe5f825f7fcc467762d1a6d34e5a854872fa
mostafa mahmoud <mostafamahmoud9619@gmail.com>
2023-10-30 12:40:21 +0200

    git commit update users url

--- users/urls.py
+++ users/urls.py
@@ -9,7 +9,7 @@ from rest_framework_simplejwt.views import (
 app_name = 'users'
 
 urlpatterns = [
-    path('signup/', views.UserCreateApiView.as_view(), name='signup'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/signup/', views.UserCreateApiView.as_view(), name='signup'),
+    path('user/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
