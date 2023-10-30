b8b3b25279b4d49b4cbd77444f2ac97192df0aea
mostafa mahmoud <mostafamahmoud9619@gmail.com>
2023-10-30 12:43:26 +0200

    add exchange rate model adminmodel

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Transaction
+from .models import Transaction, ExchangeRate
 
 # write your admin models here
 
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
     model = Transaction
+
+@admin.register(ExchangeRate)
+class TransactionAdmin(admin.ModelAdmin):
+    model = ExchangeRate
