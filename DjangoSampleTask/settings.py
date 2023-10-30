2bbca834f39d0d90fe734a4b8f6d0597f22b1e6c
mostafa mahmoud <mostafamahmoud9619@gmail.com>
2023-10-30 16:59:16 +0200

    remove custom logging

--- DjangoSampleTask/settings.py
+++ DjangoSampleTask/settings.py
@@ -126,24 +126,6 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-LOGGING = {
-    "version": 1,  # the dictConfig format version
-    "disable_existing_loggers": False,  # retain the default loggers
-    "handlers": {
-        "file": {
-            "class": "logging.FileHandler",
-            "filename": "general.log",
-            "level": "DEBUG",
-            "formatter": "verbose",
-        },
-    },
-    "formatters": {
-        "verbose": {
-            "format": "{name} {levelname} {asctime} {module} {process:d} {thread:d} {message}",
-            "style": "{",
-        }
-    }
-}
 
 # Rest Framework Settings
 REST_FRAMEWORK = {
@@ -167,7 +149,9 @@ REST_FRAMEWORK = {
     'DEFAULT_VERSION': 'v1',
     # pagination
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    # Request Test format
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 }
 
 
