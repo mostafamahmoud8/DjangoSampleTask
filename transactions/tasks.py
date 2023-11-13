5b2f39a9aa439854230bb3d267c28b911f49a9cb
mostafa mahmoud <mostafamahmoud9619@gmail.com>
2023-11-13 09:55:55 +0200

    refactoring

--- transactions/tasks.py
+++ transactions/tasks.py
@@ -8,15 +8,17 @@ import datetime
 
 logger = get_task_logger(__name__)
 
-DELETE_TEMP_USER_TIME=60*60*48 # 48 hours
+CHANGE_TASK_STATUS=60*60*48 # 48 hours
 
 
 @shared_task()
 def change_transaction_status():
-     date_delta = timezone.now().date() - datetime.timedelta(seconds=DELETE_TEMP_USER_TIME)
+     date_delta = timezone.now().date() - datetime.timedelta(seconds=CHANGE_TASK_STATUS)
      transactions = Transaction.objects.filter(created_at__date__lte=date_delta)
      transactions.update(status=Transaction.TRANSACTIONSTATUS.PERMANENT)
-     return str(len(transactions)) + " transactions has been affected by celery task"
+     msg = str(len(transactions)) + " transactions has been affected by celery task"
+     logger.info(msg)
+     return msg
 
  
  
